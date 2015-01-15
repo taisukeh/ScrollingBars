41b1d8c342851cd99a8f120703cf239e045c37c9
Taisuke Hori <tahori@yahoo-corp.jp>
2015-01-15 18:48:29 +0900

    fix for retest

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 set -o pipefail
+set -x
 
 BASEDIR=$(dirname $0)
 cd "$BASEDIR"/../ScrollingBarsTest
 
 xcodebuild test -workspace ScrollingBarsTest.xcworkspace \
       -scheme ScrollingBarsTest -sdk iphonesimulator \
-      BUILD_ACTIVE_ARCH=NO | xcpretty -s -c
+      BUILD_ACTIVE_ARCH=NO
+# | xcpretty -s -c
